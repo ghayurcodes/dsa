ea0b70f7b8766798e2797373302f4aa3809d7b5d
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-09-20 20:58:51 +0500

    Update play.java

--- play.java
+++ play.java
@@ -4,8 +4,14 @@ import java.util.*;
 public class play {
     public static void main(String[] args) {
         int[] arr={23,44};
-        arr.s
         System.out.println( Arrays.toString(arr));
+        boolean f=false;
+        for(int i=0;i<3;i++){
+            System.out.println("hey");
+            if(f==false){
+                break;
+            }
+        }
 
     }
 }
