b9aed077a3095d94bc2617fe66bdfe3abf489f2d
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-09-23 18:03:01 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,10 +4,10 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("hello");
-        Scanner in =new Scanner(System.in);
-        String name=in.next();
-        System.out.println("your name is "+name);
-        in.close();
+        try (Scanner in = new Scanner(System.in)) {
+            String name=in.next();
+            System.out.println("your name is "+name);
+        }
     
 
         
