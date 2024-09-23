cdf68b9fe3c2e0ed5a106c0c4fb6e1c4073d9a67
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-09-23 19:44:45 +0500

    Create arays.java

---
+++ arays.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class arays {
+    public static void main(String[] args) {
+        // int arr[]=new int[3];
+        // int[][] ary=new int[2][3];
+        // arr[2]=99;
+        // arr[0]=9;
+       
+        
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        String first="ghayur";
+        String last="ahayur";
+        System.out.println(first.substring(3, 6));
+        // System.out.println("pls enter something");
+        // Scanner in=new Scanner(System.in);
+        // String g=in.nextLine();
+        // System.out.println("you typed : "+g);
+
+        
+
+    }
+}
