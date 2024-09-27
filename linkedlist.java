8bd18593d1e7d282893a688b7881aeac34f77c0d
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-09-27 21:55:33 +0500

    Update linkedlist.java

--- linkedlist.java
+++ linkedlist.java
@@ -1,10 +1,17 @@
-
-
+import java.util.LinkedList;
 
 public class linkedlist {
     
     public static void main(String[] args) {
-       
-       
+       LinkedList<Integer> list=new LinkedList<>();
+       list.add(3);
+       list.add(5);
+       list.add(7);
+       System.out.println(list);
+    //    list.remove(1);
+    
+    System.out.println(list.contains(1));
+
+
     }
 }
