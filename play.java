87dd7b9d17d126ab94a1bff45db6b5ca62b4a32e
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-09-27 23:23:34 +0500

    updated

--- play.java
+++ play.java
@@ -3,8 +3,22 @@ import java.util.*;
 
 public class play {
     public static void main(String[] args) {
-        int[] arr={23,44};
-        System.out.println( Arrays.toString(arr));
+     LinkedList<Integer> l=new LinkedList<>();
+        l.add(2);
+        l.add(4);
+        l.add(6);
+        l.add(8);
+        System.out.println(l);
+
+        
+        
+
+for (int i=1;i<l.size()-1;i++) {
+            int temp=l.get(l.size()-i) ;
+            l.add(temp);
+            l.remove(l.size()-i);
+        }
+        System.out.println(l);
        
 
     }
