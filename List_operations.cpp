b9310bec97f6eb54cc8e92d71eedb6b10ac4e84d
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-07 12:52:14 +0500

    update

--- List_operations.cpp
+++ List_operations.cpp
@@ -29,11 +29,57 @@ public:
         head = nullptr;
     }
 
-    
+     void append(int data) {
+        if (head == nullptr) {
+            head = new Node(data);
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = new Node(data);
+        
+    }
+
+    void display(){
+        Node* temp=head;
+        while(temp!=nullptr){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+
+    int size(){
+        Node* temp=head;
+        int count=0;
+        while(temp!=nullptr){
+            temp=temp->next;
+            count++;
+
+
+        }
+     
+        return count;
+    }
+
+
 
 };
 
 int main(){
+LinkedList list;
+
+list.append(3);
+list.append(4);
+list.append(9);
+list.append(7);
+list.append(2);
+
+//displaying
+list.display();
+cout<<endl;
 
     
 }
