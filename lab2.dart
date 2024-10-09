b263d2204343fef76fb46d162f67ec97634b6e13
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-09 11:22:00 +0500

    update

--- lab2.dart
+++ lab2.dart
@@ -1,15 +1,27 @@
 import 'dart:io';
 
+// void main() {
+//   stdout.write("Enter a number");
+//   int num = int.parse(stdin.readLineSync()!);
+//   for (var i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+
+
 void main() {
-  stdout.write("Enter a number");
-  int num=int.parse(stdin.readLineSync()!);
-  if(num%2==0){
-    print("Number is even");
+  print(factorial(6));
+}
+
+int factorial(int number) {
+  if (number <= 0) {
+    // termination case
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+    // function invokes itself
   }
-  else{
-    print("Number is odd");
-  }
-
-
-
 }
