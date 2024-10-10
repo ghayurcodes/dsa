4c89f1c6e579b7c756304a856b58c4dad6025f0c
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-10 18:28:41 +0500

    upate

--- single-List_operations.cpp
+++ single-List_operations.cpp
@@ -2,22 +2,18 @@
 
 using namespace std;
 
-
-
 class Node {
 public:
     int data;
     Node* next;
-   
 
     Node(int data) {
-       this->data = data;
+        this->data = data;
         this->next = nullptr;
-        
     }
 
-    ~Node(){
-        cout<<"index deleted with value :"<<this->data<<endl;
+    ~Node() {
+        cout << "Node deleted with value: " << this->data << endl;
     }
 };
 
@@ -29,7 +25,7 @@ public:
         head = nullptr;
     }
 
-     void append(int data) {
+    void append(int data) {
         if (head == nullptr) {
             head = new Node(data);
             return;
@@ -40,192 +36,157 @@ public:
             temp = temp->next;
         }
         temp->next = new Node(data);
-        
     }
 
-    void display(){
-        Node* temp=head;
-        while(temp!=nullptr){
-            cout<<temp->data<<" ";
-            temp=temp->next;
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
         }
     }
 
-    int size(){
-        Node* temp=head;
-        int count=0;
-        while(temp!=nullptr){
-            temp=temp->next;
+    int size() {
+        Node* temp = head;
+        int count = 0;
+        while (temp != nullptr) {
+            temp = temp->next;
             count++;
-
-
         }
-     
         return count;
     }
 
+    void reverse() {
+        Node* curr = head;
+        Node* prev = nullptr;
+        Node* next = nullptr;
 
-    void reverse(){
-        Node* curr=head;
-        Node* prev=nullptr;
-        Node* next=nullptr;
-
-        while(curr!=nullptr){
-            next=curr->next; //most basic code
-            curr->next=prev;
-            prev=curr; 
-            curr=next;
-        
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        head=prev;
-
-
+        head = prev;
     }
 
+    void reverse_recursion(Node*& prev, Node*& curr) {
+        if (curr == NULL) {
+            head = prev;
+            return;
+        }
+        Node* next = curr->next;
+        reverse_recursion(curr, next);
+        curr->next = prev;
+    }
 
-    void reverse_recurcion(Node*& prev,Node*& curr){//pasing by referanec tahts why using & sign
-            if(curr==NULL){
-                head=prev;
-                return;
+    void middle() {
+        Node* temp = head;
+        for (int i = 0; i < size() / 2; i++) {
+            temp = temp->next;
+        }
+        cout << "Middle element at index: " << size() / 2 << " Element: " << temp->data << endl;
+    }
+
+    bool exists(int element) {
+        Node* temp = head;
+        while (temp != nullptr) {
+            if (temp->data == element) {
+                return true;
             }
-            Node* next=curr->next;
-            reverse_recurcion(curr,next);
-            curr->next=prev;
-        
-
-    }
-
-
-
-    void middle(){
-       
-        Node* temp=head;
-        for(int i=0;i<size()/2;i++){
-            temp=temp->next;
+            temp = temp->next;
         }
-        cout<<"middel element at index: "<<size()/2<<" Element: "<<temp->data<<endl;
-
-
-
+        return false;
     }
 
-    bool exists(int element){
-        Node* temp=head;
-        bool exists=false;
+    void remove(int element) {
+        if (head == nullptr) return;
 
-        while(temp!=nullptr){
-            if(temp->data==element){
-                exists=true;
-                return exists;
-                break;
-            }
-            else{
-                temp=temp->next;
-            }
-            
-
+        // Special case for head
+        if (head->data == element) {
+            Node* toDelete = head;
+            head = head->next;
+            delete toDelete;
+            return;
         }
-      
 
-        return exists;
-        
-    }
-
-
-    void remove(int element){
-        Node* temp=head;
-
-        while(temp->next->data!=element){
-            temp=temp->next;
-
+        Node* temp = head;
+        while (temp->next != nullptr && temp->next->data != element) {
+            temp = temp->next;
         }
-        Node* To_delete=temp->next;
-        temp->next=temp->next->next;
-        delete To_delete;
 
-
-    }
-    
-
-
-
-    void join(LinkedList* list1,LinkedList* list2){
-        Node* temp1=list1->head;
-         Node*temp2=list2->head;
-
-         while(temp1->next!=nullptr){
-            temp1=temp1->next;
-
-         }
-         temp1->next=temp2;
-
-         
-
+        if (temp->next != nullptr) {
+            Node* toDelete = temp->next;
+            temp->next = temp->next->next;
+            delete toDelete;
+        }
     }
 
+    void join(LinkedList* list1, LinkedList* list2) {
+        Node* temp1 = list1->head;
+        Node* temp2 = list2->head;
 
-   Node* k_reverse(Node* head,int k){
+        while (temp1->next != nullptr) {
+            temp1 = temp1->next;
+        }
+        temp1->next = temp2;
+    }
 
-        if(head==NULL){
+    Node* k_reverse(Node* head, int k) {
+        if (head == NULL) {
             return NULL;
         }
-        Node* curr=head;
-        Node* prev=nullptr;
-        Node* frwrd=nullptr;
-        int count=0;
 
-         while(curr!=nullptr && count<k){
-            frwrd=curr->next; 
-            curr->next=prev;
-            prev=curr; 
-            curr=frwrd;
+        Node* curr = head;
+        Node* prev = nullptr;
+        Node* frwrd = nullptr;
+        int count = 0;
+
+        // Reverse first 'k' nodes
+        while (curr != nullptr && count < k) {
+            frwrd = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = frwrd;
             count++;
         }
-        
 
+        // Recursively reverse the rest of the list
+        if (frwrd != NULL) {
+    
+            head->next = k_reverse(frwrd, k);
+        }
+
+        // Return the new head
+        return prev;
     }
-
-
 };
 
-int main(){
-LinkedList list;
+int main() {
+    LinkedList list;
 
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-list.append(5);
-list.append(6);
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+    list.append(6);
 
+    LinkedList list2;
+    list2.append(88);
+    list2.append(99);
 
-LinkedList list2;
-list2.append(88);
-list2.append(99);
+    // Displaying list before reversing
+    list.display();
+    cout << endl;
 
-
-//displaying
-list.display();
-cout<<endl;
-
-
-// Node* curr=list.head;
-// Node*prev= nullptr;
-// list.reverse_recurcion(prev,curr); recursive call
-
-
-
-// list.middle();
-
-
-// cout<<list.exists(122);
-// cout<<endl;
-// list.remove(12);
-// list.display();
-// list.join(&list,&list2);
-// list.display();
-
-cout<<endl;
+    // k reverse call
+    list.head = list.k_reverse(list.head, 2);
     
-}
+    // Display after reverse
+    cout << "After k-reverse:" << endl;
+    list.display();
+    cout << endl;
 
+    return 0;
+}
