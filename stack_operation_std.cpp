5c69d408988d0010383b4a98b6b6264bcb2279bf
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-14 20:36:52 +0500

    update

--- stack_operation_std.cpp
+++ stack_operation_std.cpp
@@ -10,5 +10,18 @@ int main(){
     string word="hello";
 
     stack<char> s;
-    
+
+    for(int i=0;i<word.length();i++){
+        char j=word[i];
+        s.push(j);
+    }
+    string ans="";
+    while(!s.empty()){
+        ans+=s.top();
+        s.pop();
+    }
+
+
+    cout<<"reversd string is: "<<ans<<endl;
+
 }
