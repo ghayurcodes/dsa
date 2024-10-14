4878f2fc0f1102caf18d727f1a70bc5b7284b1a6
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-14 20:37:09 +0500

    Update stack_operation_std.cpp

--- stack_operation_std.cpp
+++ stack_operation_std.cpp
@@ -7,13 +7,15 @@ using namespace std;
 
 
 int main(){
+
+
     string word="hello";
 
     stack<char> s;
 
     for(int i=0;i<word.length();i++){
         char j=word[i];
-        s.push(j);
+        s.push(j);                          //tyo reverse a string using stack
     }
     string ans="";
     while(!s.empty()){
