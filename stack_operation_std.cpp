18fd4a444320860ffdcf1948ebd28d2ff3e97886
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-14 20:45:36 +0500

    update

--- stack_operation_std.cpp
+++ stack_operation_std.cpp
@@ -4,26 +4,46 @@
 using namespace std;
 
 
+void delet__middle( stack<int>& s){
+ 
+    s.pop();
 
+}
 
 int main(){
 
 
-    string word="hello";
+    // string word="hello";
 
-    stack<char> s;
+    // stack<char> s;
 
-    for(int i=0;i<word.length();i++){
-        char j=word[i];
-        s.push(j);                          //tyo reverse a string using stack
-    }
-    string ans="";
-    while(!s.empty()){
-        ans+=s.top();
-        s.pop();
-    }
+    // for(int i=0;i<word.length();i++){
+    //     char j=word[i];
+    //     s.push(j);                          //tyo reverse a string using stack
+    // }
+    // string ans="";
+    // while(!s.empty()){
+    //     ans+=s.top();
+    //     s.pop();
+    // }
+    // cout<<"reversd string is: "<<ans<<endl;
+    // stack<int> n;
+
+    // n.push(4);
+    // n.push(6);
+    // n.push(9);
+    // n.push(11);
+    // n.push(15);
 
 
-    cout<<"reversd string is: "<<ans<<endl;
+
+
+    delet__middle(n);
+
+    cout<<n.top();
+
+
+
+
 
 }
