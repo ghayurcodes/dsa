6c3315ae2532f18c2b2f2d21f110562e6f6dfc4e
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-16 11:30:07 +0500

    Update labtemp.dart

--- labtemp.dart
+++ labtemp.dart
@@ -1,87 +1,67 @@
+class User {
+  String _name;
+  String _email;
+  String _password;
 
+  User(this._name, this._email, this._password);
 
-class Vehicle {
-  String make;
-  String model;
-  int year;
-  double mileage;
+  String get name => _name;
+  set name(String name) => _name = name;
 
-  Vehicle(this.make, this.model, this.year, this.mileage);
+  String get email => _email;
+  set email(String email) => _email = email;
+
+  String get password => _password;
+  set password(String password) => _password = password;
 
   void displayInfo() {
-    print('Make: $make, Model: $model, Year: $year, Mileage: $mileage');
-  }
-}
-
-class Car extends Vehicle {
-  int seats;
-  String fuelType;
-
-  Car(String make, String model, int year, double mileage, this.seats, this.fuelType)
-      : super(make, model, year, mileage);
-
-  
-  void displayInfo() {
-    super.displayInfo();
-    print('Seats: $seats, Fuel Type: $fuelType');
+    print('Name: $_name, Email: $_email');
   }
 }
 
 
-class Motorcycle extends Vehicle {
-  double engineCapacity;
-  String type;
+class AdminUser extends User {
+  AdminUser(String name, String email, String password) : super(name, email, password);
 
-  Motorcycle(String make, String model, int year, double mileage, this.engineCapacity, this.type)
-      : super(make, model, year, mileage);
+  void manageSystem() {
+    print('Admin privileges granted.');
+  }
 
   @override
   void displayInfo() {
     super.displayInfo();
-    print('Engine Capacity: $engineCapacity cc, Type: $type');
+    print('User Role: Admin');
   }
 }
 
 
-class Garage {
-  List<Vehicle> vehicles = [];
+class RegularUser extends User {
+  RegularUser(String name, String email, String password) : super(name, email, password);
 
-  void addVehicle(Vehicle vehicle) {
-    vehicles.add(vehicle);
-    print('${vehicle.model} added to garage.');
+  void browseContent() {
+    print('Browsing as a regular user.');
   }
 
-  void removeVehicle(String model) {
-    vehicles.removeWhere((vehicle) => vehicle.model == model);
-    print('$model removed from garage.');
-  }
-
-  void showAllVehicles() {
-    print('Vehicles in the garage:');
-    for (var vehicle in vehicles) {
-      vehicle.displayInfo();
-    }
+  @override
+  void displayInfo() {
+    super.displayInfo();
+    print('User Role: Regular');
   }
 }
-
 
 
 void main() {
- 
-  Car car1 = Car('Toyota', 'Corolla', 2020, 15000, 5, 'Petrol');
-  Motorcycle moto1 = Motorcycle('Harley', 'Sportster', 2018, 8000, 1200, 'Cruiser');
+  // Create Admin and Regular users
+  AdminUser admin = AdminUser('Admin', 'admin@example.com', 'adminPass');
+  RegularUser regularUser = RegularUser('John Doe', 'john@example.com', 'userPass');
 
- 
-  Garage garage =  Garage();
-  garage.addVehicle(car1);
-  garage.addVehicle(moto1);
+  // Display user info
+  admin.displayInfo();
+  regularUser.displayInfo();
 
- 
-  garage.showAllVehicles();
+  // Admin performs management actions
+  admin.manageSystem();
 
- 
-  garage.removeVehicle('Corolla');
-
-
-  garage.showAllVehicles();
+  // Regular user browses content
+  regularUser.browseContent();
 }
