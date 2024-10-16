b89cc16efe0e4c3df9cc49daf7c5b80120766fa6
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-16 11:18:13 +0500

    Update labtemp.dart

--- labtemp.dart
+++ labtemp.dart
@@ -1,1 +1,28 @@
 
+
+class Vehicle {
+  String make;
+  String model;
+  int year;
+  double mileage;
+
+  Vehicle(this.make, this.model, this.year, this.mileage);
+
+  void displayInfo() {
+    print('Make: $make, Model: $model, Year: $year, Mileage: $mileage');
+  }
+}
+
+class Car extends Vehicle {
+  int seats;
+  String fuelType;
+
+  Car(String make, String model, int year, double mileage, this.seats, this.fuelType)
+      : super(make, model, year, mileage);
+
+  @override
+  void displayInfo() {
+    super.displayInfo();
+    print('Seats: $seats, Fuel Type: $fuelType');
+  }
+}
