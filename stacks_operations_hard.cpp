7f48b88a7cc054450b35f84251759e18548b687f
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-17 21:36:12 +0500

    update

--- stacks_operations_hard.cpp
+++ stacks_operations_hard.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 #include<stack>
+#include<vector>
 
 using namespace std;
 
+void find_next_smaller_element( vector<int>& arr){
+    stack<int> s;
+    s.push(-1);
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]<s.top()){
+            
+        }
+    }
+
+}
+
+
 int main(){
-    cout<<"hello";
+
+    vector<int> arr;
+    arr.push_back(5);
+    arr.push_back(6);
+    arr.push_back(2);                   //next samller element
+    arr.push_back(3);
+
+    find_next_smaller_element(arr);
+
+
 }
