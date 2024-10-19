348fb2cc7f1f3dae5724131b5c11b92b38c744a2
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-19 16:12:26 +0500

    Update circural_que.cpp

--- circural_que.cpp
+++ circural_que.cpp
@@ -21,29 +21,40 @@ public:
 
 
     void push(int data){
-        if(rear==size){
-            cout<<"overflowed \n";
-        }
-        else{
-            arr[rear]=data;
+       if((qfront==0 && rear==size-1)||(rear==(qfront-1)%(size-1))){
+        cout<<"que is full\n";
+       }
+       else if(qfront==-1){
+        qfront=rear=0;
+        arr[rear]=data;
+    
+       }
+       else if(rear==size-1 && qfront!=0){
+        rear=0;
+        arr[rear]=data;
+       }
+       else{
+        arr[rear]=data;
             rear++;
-        }
+       }
     }
 
 
 
     void pop(){
-        if(qfront==rear){
-            cout<<"underflowed que is empty\n";
-        }
-        else{
-            arr[qfront]=-1;
-            qfront++;
-            if(qfront==rear){
-                qfront=0;
-                rear=0;
-            }
-        }
+       if(qfront==-1){
+        cout<<"quue ois empty\n";//emopty que
+
+       }
+       else if(qfront==rear){//pnly 1 element prest in que
+        qfront=rear=-1;
+       }
+       else if(qfront==size-1){//to maintain cyclic nature
+        qfront =0;
+       }
+       else{
+        qfront++;//nprmal que
+       }
     }
 
 
