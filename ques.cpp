c31b87d451e1839bd6f0352c79a7a490459f568b
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-19 12:26:54 +0500

    Update ques.cpp

--- ques.cpp
+++ ques.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <queue>            //fifo (first in first out)
+                                             //fifo (first in first out)
 
 using namespace std;
 
+class que{
+    int front;
+    int rear;
+    int size;
+    int *arr;
 
+
+
+    que(int size){
+        this->size=0;
+        arr=new int[size];
+        this->front=0;
+        this->rear=0;
+    }
+
+
+
+    void push(int data){
+        if(rear==size){
+            cout<<"overflowed \n";
+        }
+        else{
+
+        }
+    }
+};
 
 int main() {
     
