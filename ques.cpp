7e50634e17af70f8a8bad7831b1cf73fcf97712e
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-19 12:40:02 +0500

    update

--- ques.cpp
+++ ques.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 class que{
-    int front;
+    int qfront;
     int rear;
     int size;
     int *arr;
@@ -14,7 +14,7 @@ class que{
     que(int size){
         this->size=0;
         arr=new int[size];
-        this->front=0;
+        this->qfront=0;
         this->rear=0;
     }
 
@@ -25,7 +25,45 @@ class que{
             cout<<"overflowed \n";
         }
         else{
+            arr[rear]=data;
+            rear++;
+        }
+    }
 
+
+
+    void pop(){
+        if(qfront==rear){
+            cout<<"underflowed que is empty\n";
+        }
+        else{
+            arr[qfront]=-1;
+            qfront++;
+            if(qfront==rear){
+                qfront=0;
+                rear=0;
+            }
+        }
+    }
+
+
+    int front(){
+        if(qfront==rear){
+            cout<<"empty que";
+            return -1;
+        }
+        else{
+            return arr[qfront];
+        }
+    }
+
+
+    bool isempty(){
+        if(qfront==rear){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 };
