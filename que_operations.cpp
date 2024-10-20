ee1c0a8e20df7fd58a800d5dc0052640d2562347
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-20 18:31:29 +0500

    update

--- que_operations.cpp
+++ que_operations.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
-
+#include<queue>
 using namespace std;
+
+
+
 int main(){
-    cout<<"hella\n";
+    queue<int> q;
+    q.push(2);
+    q.push(3);
+    q.push(8);
+    q.push(1);
+    q.push(5);
+
+    cout<<"front :"<<q.front()<<" rear: "<<q.back();
+    cout<<"size: "<<q.size();
+    cout<<endl;
+    q.pop();
+    cout<<endl;
+    cout<<"front :"<<q.front()<<" rear: "<<q.back();
 }
