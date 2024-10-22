45066fb929c363e83fcbbf13debdc8acac4defd3
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-22 22:02:22 +0500

    update

--- play.cpp
+++ play.cpp
@@ -3,12 +3,6 @@
 using namespace std;
 
 
-void display(vector<int>& arr){
-    for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
 
 
 int main() {
@@ -36,11 +30,6 @@ int main() {
 
     // return 0;
 
-    stack<int> s;
-    s.push(3);
-    s.push(4);
-    s.push(5);
-    s.push(6);
-    s.push(6);
-
+   
+    
 }
