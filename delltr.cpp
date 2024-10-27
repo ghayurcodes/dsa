c44ef946ec0f2da017bca7350c0dbfae3c951e8a
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-27 17:38:55 +0500

    Create delltr.cpp

---
+++ delltr.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+class Student{
+    int age;
+    int roll;
+    float cgpa;
+    string name;
+    Student* nxtstdred;  ///next student record
+   
+    //defualt constr..
+    ///set
+    ///get
+
+   
+};
+
+class StdList{
+    Student *head;
+    Student *current;
+    int noR;  /// no of records
+   
+    public:
+        //const
+        // other functions
+       
+       
+};
+
+
+int main(){
+   
+    cout<<"\n\t=== Student Menu ===";
+    cout<<"\n\t1. Add Record";
+    cout<<"\n\t2. Delete Record";
+    cout<<"\n\t3. Update Record";
+    cout<<"\n\t4. Search Record";
+    cout<<"\n\t5. Exit";
+   
+    ////
+
+   
+   
+
+}
