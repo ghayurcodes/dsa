d61d78d9706632629a43e58010830baad81bed3c
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-11-19 20:43:05 +0500

    up

--- lab18nov/task1.cpp
+++ lab18nov/task1.cpp
@@ -1,7 +1,70 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"hey\n";
+class SinglyLinkedList {
+private:
+    class Node {
+    public:
+        int data;
+        Node* next;
+        Node(int val) : data(val), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+        while (current) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    void display() const {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+
+    cout << "Original List: ";
+    list.display();
+
+    list.reverse();
+
+    cout << "Reversed List: ";
+    list.display();
+
+    return 0;
 }
