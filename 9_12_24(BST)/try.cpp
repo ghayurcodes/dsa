bc0489fc0011f3af392061d91a4f8c4244ead9c0
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-12-09 20:13:15 +0500

    u

---
+++ 9_12_24(BST)/try.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data=d;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+
+
+
+
+
+
+
+void takeinput(){
+    
+}
+
+
+
+
+
+int main(){
+    
+    node* root=nullptr;
+
+    cout<<"enter data:\n";
+    takeinput();
+
+}
