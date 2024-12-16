24904b04206d58c2b7281e016790a33f5e1695e1
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-12-16 20:43:21 +0500

    update

--- 16_12_24_lab/task1.cpp
+++ 16_12_24_lab/task1.cpp
@@ -4,90 +4,72 @@
 
 using namespace std;
 
+class heap{
+    public:
+    int size;
+    int* arr;
+    int current;
 
-class MaxHeap {
-private:
-    vector<int> heap;
+    heap(int size){
+        this->size=size;
+        arr=new int[size];
+        current=0;
+    }
 
-    void heapifyUp(int index) {
-        while (index > 0) {
-            int parent = (index - 1) / 2;
-            if (heap[index] > heap[parent]) {
-                swap(heap[index], heap[parent]);
-                index = parent;
-            } else {
-                break;
+
+    void add(int val){
+        if(current==size){
+        cout<<"array is full\n";
+        return;
+        }
+
+        arr[current]=val;
+        int index=current;
+        current++;
+        while(index!=0){
+            int parent=(index-1)/2;
+
+            if(arr[parent]<arr[index]){
+                swap(arr[parent],arr[index]);
+                index=parent;
+            }
+            else{
+                
+                return;
+               
             }
         }
+
+
     }
 
-    void heapifyDown(int index) {
-        int size = heap.size();
-        while (index < size) {
-            int left = 2 * index + 1;
-            int right = 2 * index + 2;
-            int largest = index;
 
-            if (left < size && heap[left] > heap[largest]) {
-                largest = left;
-            }
-            if (right < size && heap[right] > heap[largest]) {
-                largest = right;
-            }
-            if (largest != index) {
-                swap(heap[index], heap[largest]);
-                index = largest;
-            } else {
-                break;
-            }
+
+    void display(){
+        for(int i=0;i<current;i++){
+            cout<<arr[i]<<" ";
         }
+        cout<<endl;
     }
 
-public:
-    void insert(int value) {
-        heap.push_back(value);
-        heapifyUp(heap.size() - 1);
+    int maxnum(){
+        return arr[0];
     }
 
-    int extractMax() {
-        if (heap.empty()) {
-            throw runtime_error("Heap is empty");
-        }
-        int maxValue = heap[0];
-        heap[0] = heap.back();
-        heap.pop_back();
-        heapifyDown(0);
-        return maxValue;
-    }
 
-    void display() {
-        for (int value : heap) {
-            cout << value << " ";
-        }
-        cout << endl;
-    }
 };
 
-int main() {
-    MaxHeap maxHeap;
-
-   
-    maxHeap.insert(10);
-    maxHeap.insert(5);
-    maxHeap.insert(20);
-    maxHeap.insert(15);
-
-    cout << "Max Heap after inserting 10, 5, 20, 15: ";
-    maxHeap.display();
+int main(){
+    heap h(10);
+    h.add(10);
+    h.add(5);
+    h.add(20);
+    h.add(15);
 
 
-    cout << "Extracted max: " << maxHeap.extractMax() << endl;
-    cout << "Max Heap after extracting max: ";
-    maxHeap.display();
+    cout<<"displaying all:\n";
+    h.display();
 
-    cout << "Extracted max: " << maxHeap.extractMax() << endl;
-    cout << "Max Heap after extracting max: ";
-    maxHeap.display();
-
-    return 0;
+    cout<<"\nMax number in heap: "<<h.maxnum()<<" "<<h.maxnum()<<endl;
 }
+
