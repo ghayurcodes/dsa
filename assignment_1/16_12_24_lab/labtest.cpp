29948fd8cde7e26fda80ab2022f95967b37fec0b
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-12-16 14:24:56 +0500

    u

--- assignment_1/16_12_24_lab/labtest.cpp
+++ assignment_1/16_12_24_lab/labtest.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using  namespace std;
+using namespace std;
 
 class minHeap {
 public:
@@ -12,17 +12,67 @@ public:
         capacity = cap;
         arr = new int[cap];
     }
+
+    // Function to insert a new value into the heap
+    void insert(int val) {
+        if (heap_size == capacity) {
+            cout << "Heap is full. Cannot insert more elements.\n";
+            return;
+        }
+
+        // Insert the new value at the end of the heap
+        heap_size++;
+        int i = heap_size - 1;
+        arr[i] = val;
+
+        // Fix the min-heap property if it's violated
+        while (i != 0 && arr[(i - 1) / 2] < arr[i]) {
+            swap(arr[i], arr[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
+
+    // Function to display the heap elements
+    void display() {
+        if (heap_size == 0) {
+            cout << "Heap is empty.\n";
+            return;
+        }
+        cout << "Heap elements: ";
+        for (int i = 0; i < heap_size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    // Function to search for a value in the heap
     void linearSearch(int val) {
         for (int i = 0; i < heap_size; i++) {
-            if (arr[i] == val)
-            {
-                cout << "value found\n";
+            if (arr[i] == val) {
+                cout << "Value found\n";
                 return;
             }
         }
+        cout << "Value not found\n";
     }
 };
-int main()
-{
-    cout << "Hello World!\n";
+
+int main() {
+    minHeap heap(10);
+
+    // Insert some elements into the heap
+    heap.insert(15);
+    heap.insert(10);
+    heap.insert(20);
+    heap.insert(5);
+    heap.insert(7);
+
+    // Display the heap elements
+    heap.display();  // Output: Heap elements: 5 7 20 15 10
+
+    // Search for a value
+    heap.linearSearch(10); // Output: Value found
+    heap.linearSearch(25); // Output: Value not found
+
+    return 0;
 }
---
+++ assignment_1/16_12_24_lab/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+    while (i != 0 && arr[(i - 1) / 2] > arr[i]) {
+            swap(arr[i], arr[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
