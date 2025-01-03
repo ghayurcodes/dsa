2d0e70efde51cd8a80aad2431ce6253e7d4c43da
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-01-03 19:22:32 +0500

    Update hofman.cpp

--- DSA_project/hofman.cpp
+++ DSA_project/hofman.cpp
@@ -242,6 +242,7 @@ int main() {
                     cerr << "\nError opening input file!\n";
                     continue;
                 }
+
                 cout << "Enter compressed file name (without extension): ";
                 cin >> outputFileName;
 
