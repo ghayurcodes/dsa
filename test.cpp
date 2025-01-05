26e4fb77cae5f8650f04ffbe54bd758223428e64
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-01-05 16:02:13 +0500

    t

--- test.cpp
+++ test.cpp
@@ -1,81 +1,94 @@
-#include <iostream>
+#include<iostream>
+
 
 using namespace std;
-
-// Simple binary tree node class
-class node {
-public:
+//simple binary tree
+class node{
+    public:
     int val;
     node* left;
     node* right;
 
-    node(int v) {
-        this->val = v;
-        left = nullptr;
-        right = nullptr;
+
+    node(int v){
+        this->val=v;
+        left=nullptr;
+        right=nullptr;
     }
+    node(){
+        left=nullptr;
+        right=nullptr;
+    }
+
+    
 };
 
-// Function to add nodes to the binary tree
-node* add() {
-    int val;
-    cout << "Enter value (-1 for no node): ";
-    cin >> val;
+node* add(){
 
-    if (val == -1) {
+    int val;
+    cout<<"enter val:(-1 to exit)\n";
+    cin>>val;
+    if(val==-1){
         return nullptr;
     }
+    node* n=new node(val);
+    
+    cout<<"enter for left of "<<val<<endl;
+    n->left=add();
+    cout<<"enter for right of "<<val<<endl;
+    n->right=add();
 
-    node* newNode = new node(val);
-    cout << "Adding left child of " << val << endl;
-    newNode->left = add();
-    cout << "Adding right child of " << val << endl;
-    newNode->right = add();
 
-    return newNode;
-}
 
-// Function to view the tree (inorder traversal)
-void view(node* root) {
-    if (root == nullptr) {
+    return n;
+    }
+
+
+
+void view(node* root){
+
+    if(root==nullptr){
         return;
     }
 
-    view(root->left);
-    cout << root->val << " ";
-    view(root->right);
-}
+    if(root->left){
+        view(root->left);
+    }
+    cout<<root->val<<" ";
 
-int main() {
-    node* root = nullptr;
-
-    while (true) {
-        cout << "\nWelcome\n";
-        cout << "1 to add\n2 to view\n3 to exit\n";
-        int opt;
-        cin >> opt;
-
-        if (opt == 3) {
-            exit(0);
-        } else if (opt == 1) {
-            if (root == nullptr) {
-                cout << "Creating a new tree.\n";
-                root = add();
-            } else {
-                cout << "Tree already exists. Cannot add root again.\n";
-            }
-        } else if (opt == 2) {
-            if (root == nullptr) {
-                cout << "Tree is empty. Nothing to view.\n";
-            } else {
-                cout << "Tree (inorder traversal): ";
-                view(root);
-                cout << endl;
-            }
-        } else {
-            cout << "Invalid option. Try again.\n";
-        }
+    if(root->right){
+        view(root->right);
     }
 
-    return 0;
+}
+
+int main(){
+    node* root=nullptr;
+    while (true)
+    {
+        
+        cout<<"\nWelcome\n";
+        cout<<"1 to add\n2 to view\n3 to exit";
+        int opt;
+        cin>>opt;
+        if(opt==3){
+            exit(0);
+        }
+        else{
+            if (opt==1)
+            {
+                root=add();
+            }
+            if(opt==2){
+                view(root);
+            }
+            
+        }
+
+    }
+    
+    
+
+
+
 }
