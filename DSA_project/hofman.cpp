1e17a28ea5680bd1e8cc94f1ab10310a28ec851f
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-01-07 10:06:18 +0500

    Update hofman.cpp

--- DSA_project/hofman.cpp
+++ DSA_project/hofman.cpp
@@ -227,7 +227,7 @@ int main() {
         // Convert char to integer
         int option = choice - '0';
 
-        // Variables for filename
+        // Variables for filenames
         string inputFileName, outputFileName, codebookFileName;
 
         switch (option) {
